850ad91406a013bdc25e566aee04c82cf9eb3d12
nitinmadelyn <nitin.madelyn@gmail.com>
2021-02-18 17:42:57 +0530

    listen port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = require('./src/config');
 const app = require('./src/app');
 
-const server = app.listen(config.port, () => {
+const server = app.listen(process.env.PORT || config.port, () => {
   console.log(JSON.stringify(server.listeners()))
   console.log("Server running on port: " + config.port);
 });
